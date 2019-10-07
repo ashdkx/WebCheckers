97b5dc4dbc6a7e734d06da08ec4f5eb2187fafc8
mindstorm2009 <curl.nicholas@gmail.com>
2019-10-07 17:46:27 -0400

    Added Get and Post routes for signin and created signin webpage

---
+++ src/main/java/com/webcheckers/ui/GetSignInRoute.java
@@ -0,0 +1,39 @@
+package com.webcheckers.ui;
+
+import spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+
+/**
+ * @author Nicholas Curl
+ */
+public class GetSignInRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(GetSignInRoute.class.getName());
+
+    private final TemplateEngine templateEngine;
+
+
+    public GetSignInRoute(final TemplateEngine templateEngine){
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+        LOG.config("GetSignInRoute is initialized.");
+
+    }
+
+
+    @Override
+    public Object handle(Request request, Response response){
+
+        LOG.finer("GetHomeRoute is invoked.");
+        Map<String, Object> vm = new HashMap<>();
+
+        vm.put("title", "Sign In");
+
+
+        return templateEngine.render(new ModelAndView(vm , "signin.ftl"));
+
+    }
+}
---
+++ src/main/resources/spark/template/freemarker/signin.ftl
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></meta>
+    <meta http-equiv="refresh" content="10">
+    <title>${title} | Web Checkers </title>
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body>
+<div class="page">
+
+    <h1>Web Checkers | ${title}</h1>
+
+    <#include "nav-bar.ftl" />
+
+        <div class="body">
+            <form method="post">
+                User Name:<br>
+                <input type="text" name="username"><br><br>
+                <input type="submit" value="Submit">
+
+            </form>
+        </div>
+</div>
+</body>
+
+</html>
+
+
