d0aa53576dca67799b5cf7515d6cbe71e8cfb511
Alec Alan Jackson <alecalanjackson@gmail.com>
2019-10-08 21:29:04 -0400

    list output bug fix current user ---> currentPlayer

--- src/main/java/com/webcheckers/ui/GetHomeRoute.java
+++ src/main/java/com/webcheckers/ui/GetHomeRoute.java
@@ -68,7 +68,7 @@ public class GetHomeRoute implements Route {
     if(httpSession.attribute(CURRENT_PLAYER) != null){
       final Player player = httpSession.attribute(CURRENT_PLAYER);
       System.out.println(player.getName());
-      vm.put("currentUser",player);
+      vm.put("currentPlayer",player);
       vm.remove("message",WELCOME_MSG);
       vm.put("message",OTHER_PLAYERS_MSG);
       // print out the list of players
