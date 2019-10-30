20f43697c06a7bf1deb37d90504520614ec0dd27
mindstorm2009 <curl.nicholas@gmail.com>
2019-10-30 17:53:03 -0400

    added test board

--- src/main/java/com/webcheckers/model/GameView.java
+++ src/main/java/com/webcheckers/model/GameView.java
@@ -25,6 +25,7 @@ public class GameView {
         this.whitePlayer = player2;
         this.board = new ArrayList<>();
         initBoard();
+        testDoubleJumpSingle();
     }
 
 
@@ -115,6 +116,18 @@ public class GameView {
         return whitePlayer;
     }
 
+    private void testDoubleJumpSingle(){
+        for(int i = 0; i<8; i++){
+            for(int j=0; j<8; j++){
+                redPlayerBoard.get(i).getSpace(j).setPiece(null);
+            }
+        }
+        redPlayerBoard.get(5).getSpace(2).setPiece(Piece.redSingle);
+        redPlayerBoard.get(4).getSpace(3).setPiece(Piece.whiteSingle);
+        redPlayerBoard.get(2).getSpace(3).setPiece(Piece.whiteSingle);
+        redPlayerBoard.get(2).getSpace(5).setPiece(Piece.whiteSingle);
+        updateWhitePlayer();
 
+    }
 
 }
